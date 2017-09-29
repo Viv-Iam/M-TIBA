8c184c95473ab88937838e84e3b2cfeae79eaf46
Christine Wasike <christinewasike22@gmail.com>
2017-09-30 01:59:21 +0300

    Implementing the POST requests

---
+++ app/src/main/java/com/cav/m_tiba/Constants.java
@@ -0,0 +1,14 @@
+package com.cav.m_tiba;
+
+/**
+ * Created by wasike on 29/09/17.
+ */
+
+public class Constants {
+    public static final String MTIBA_TOKEN = BuildConfig.MTIBA_TOKEN;
+    public static final String MTIBA_BASE_URL = "http://provider-service-test.ap-southeast-1.elasticbeanstalk.com/providers";
+    public static final String MTIBA_QUERY_PARAMETER = "q";
+    public static final String MTIBA_TILL_KEY = "till";
+    public static final String MTIBA_PASSWORD = "password";
+    public static final String MTIBA_NEW_PASSWORD = "newPassword";
+}
--- app/src/main/java/com/cav/m_tiba/ui/MainActivity.java
+++ app/src/main/java/com/cav/m_tiba/ui/MainActivity.java
@@ -1,15 +1,48 @@
 package com.cav.m_tiba.ui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.BinderThread;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.cav.m_tiba.R;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+import static com.cav.m_tiba.R.id.tillEditText;
+
+/**
+ * Created by wasike on 29/09/17.
+ */
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Bind(R.id.findTillButton) Button mFindTillButton;
+    @Bind(tillEditText) EditText mTillEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mFindTillButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Log.v("click", "click");
+        if(view == mFindTillButton) {
+            String till = mTillEditText.getText().toString();
+            Intent intent = new Intent(MainActivity.this, HamburgerMenuActivity.class);
+            intent.putExtra("till", till);
+            startActivity(intent);
+        }
+
     }
 }
