64de428a0fad05eb3b5806a7a91e3a413b347ada
Vivi-Iam <vivian.opondoh@gmail.com>
2017-09-30 07:12:56 +0300

    Add java class fetchData

---
+++ app/src/main/java/Modules/fetchData.java
@@ -0,0 +1,21 @@
+package Modules;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by vivian on 30/09/17.
+ */
+
+public class fetchData extends AsyncTask<Void, Void, Void> {
+
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+    }
+
+    @Override
+    protected Void doInBackground(Void... voids) {
+        return null;
+    }
+}
